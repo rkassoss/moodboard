af580d1030f37aa16c3b2fbbdadf08a856b51cf7
Aaron Robb <airrobb@gmail.com>
2016-06-17 04:23:16 -0500

    Remove dev tools

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { compose, createStore, applyMiddleware } from 'redux'
-import { createDevTools } from 'redux-devtools'
-import LogMonitor from 'redux-devtools-log-monitor'
-import DockMonitor from 'redux-devtools-dock-monitor'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import App from './containers/App.jsx'
 import reducer from './reducer.js'
 
 require('../styles/main.scss')
-
-export const DevTools = createDevTools(
-  <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q'>
-    <LogMonitor theme='tomorrow' preserveScrollTop={false} />
-  </DockMonitor>
-)
 
 const store = createStore(
   reducer,
